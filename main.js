82ec5f3e078da82d30b670c375c8329b0e60c485
Toby Smith <toby.d.smith@outlook.com>
2021-05-05 19:07:10 +0100

    Completed fetch, need to update interface

--- main.js
+++ main.js
@@ -10,5 +10,15 @@ function handleFoodChange() {
 
 async function fetchRecipe(food) {
   //--- write your code below ---
+  const YOUR_APP_ID = "029ee2cf";
+  const YOUR_APP_KEY = "6d8d5f8fa0c32e8665b8b483ec9da9cf";
+  let searchID = document.getElementById("food-input").value;
+  const requestUrl = `https://api.edamam.com/search?q=${searchID}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+  const newRecipePromice = await fetch(requestUrl);
+  const newRecipe = await newRecipePromice.json();
+  const myRecipe = newRecipe.hits[0];
+  console.log(myRecipe);
   //--- write your code above ---
 }
+
+// https://api.edamam.com/search?q=kale&app_id=029ee2cf&app_key=6d8d5f8fa0c32e8665b8b483ec9da9cf
